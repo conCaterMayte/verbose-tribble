c86d21ff1fc086aa887e5a8e8b9493c4cd87d76f
gracebarata <59977372+gracebarata@users.noreply.github.com>
2020-03-20 16:41:48 +0000

    styling started

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -4,10 +4,10 @@ import css from "./HomePage.module.css";
 function HomePage() {
   return (
     <main className={css.main}>
-      <h1 className={css.heading}>HOME</h1>
+      <h1 className={css.heading}></h1>
       <section className={css.section}>
         <div>
-          <h1>Lightning Quiz!</h1>
+          <h1>LIGHTNING QUIZ</h1>
           <br />
           <p>Got a couple of minutes to spare?</p>
           <p>Put your knowledge to the test with our lightning-fast quiz!</p>
